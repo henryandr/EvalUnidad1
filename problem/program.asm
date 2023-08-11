8366eb158e3cfc816c37d1f84e203ecfbb4c4039
Henry Andrade <henry.andrade@upb.edu.co>
2023-08-11 11:39:54 -0500

    Se resuelve el problema en el archivo .asm y se actualizó el program.tst

--- problem/program.asm
+++ problem/program.asm
@@ -2,3 +2,94 @@
 // COLOCA AQUÍ TU ID
 // COLOCA AQUÍ TU CORREO ELECTRÓNICO
 
+(KEYBOARD)
+            @84
+            D=A
+            @KBD 
+            D=D-M 
+            @CROSS
+            D;JEQ
+
+            @67
+            D=A
+            @KBD 
+            D=D-M 
+            @CLEAR
+            D;JEQ
+            @KEYBOARD
+            0;JMP
+
+(CROSS)
+            @256
+            D=A 
+            @color1
+            M=D
+            @color2
+            M=-1
+            @PAINT
+            
+            0;JMP
+
+(CLEAR)
+            @color1
+            M=0
+            @color2
+            M=0
+            @PAINT
+            0;JMP
+
+(PAINT)
+            @16400
+            D=A
+            @screen_ver
+            M=D
+
+            @20448
+            D=A
+            @screen_hor
+            M=D
+
+            @255
+            D=A
+            @cont_ver
+            M=D
+
+            @31
+            D=A
+            @cont_hor
+            M=D
+
+(LOOP_VER)
+            @color1
+            D=M
+            @screen_ver
+            A=M
+            M=D
+            @32
+            D=A
+            @screen_ver
+            M=M+D
+            @cont_ver
+            D=M
+            M=M-1
+            @LOOP_VER
+            D;JGT
+(LOOP_HOR)
+            @color2
+            D=M
+            @screen_hor
+            A=M
+            M=D
+            @screen_hor
+            M=M+1
+            @cont_hor
+            D=M
+            M=M-1
+            @LOOP_HOR
+            D;JGT
+            @KEYBOARD
+            0;JMP
+            
+            
+
+
